b2647f9669c26dbba84d80c3642976678c1113d4
Suhas <suhasraja1@gmail.com>
2020-03-18 09:56:59 -0500

    started q3

--- hw4/q3.cu
+++ hw4/q3.cu
@@ -1,23 +1,51 @@
 #include <stdio.h>
-
-#define NUM_BLOCKS 16
+#include <string.h>
+#define NUM_BLOCKS 1
 #define BLOCK_WIDTH 1
 
-__global__ void hello()
-{
-    printf("Hello world! I'm a thread in block %d\n", blockIdx.x);
+__global__ void kern(int* inp, int inpLen, int* res, int* resLen){
+    int j=0;
+    for(int i = 0; i<inpLen; i++){
+        if(inp[i]%2==1){
+            res[j]=inp[i];
+            j++;
+        }
+    }
+    *resLen=j;
 }
 
 
-int main(int argc,char **argv)
-{
-    // launch the kernel
-    hello<<<NUM_BLOCKS, BLOCK_WIDTH>>>();
+int main(int argc,char **argv){
+    //read array in
+    char buff[50000];
+    int inp[5000];
+    buff[0]=' ';
+    char* token;
+    FILE* fp = fopen("inp.txt", "r" );
+    fgets(buff+1, 50000, fp);
+    token=strtok(buff,",");
+    int numLen=0;
+    while(token!=NULL){
+        inp[numLen]=atoi(token+1);
+        numLen++;
+        token=strtok(NULL,",");
+    }
 
-    // force the printf()s to flush
+    //transmit to GPU
+    printf("started gpu sect\n");
+    int* cudaInp;
+    int* resArr;
+    int resLen;
+    cudaMalloc((void**)&cudaInp,numLen*sizeof(int));
+    cudaMalloc((void**)&resArr,numLen*sizeof(int));
+    cudaMemcpy(cudaInp, &inp, numLen*sizeof(int), cudaMemcpyHostToDevice);
+
+    //run kernel
+    kern<<<NUM_BLOCKS, BLOCK_WIDTH>>>(cudaInp,numLen,resArr,&resLen);
     cudaDeviceSynchronize();
-
-    printf("That's all!\n");
-
-    return 0;
+    printf("%d\n",resLen);
+    for(int j=0; j<resLen;j++){
+        printf("%d\n",resArr[j]);
+    }
+    cudaFree(cudaInp);
 }
