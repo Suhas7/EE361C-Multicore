a44d87074d15180f7315856a536d739194c95338
matthew-j <42253617+matthew-j@users.noreply.github.com>
2020-03-24 17:13:23 -0500

    Finished q1, still needs testing. Fixed q2a

--- hw4/q1.cu
+++ hw4/q1.cu
@@ -3,8 +3,25 @@
 
 #define THREADS_PER_BLOCK 32
 
-__global__ void minA_cuda(int* a, int* b, int len) {
-    //Min A
+__global__ void minA_cuda(int* a, int* b, int len, int n_output) {
+	int b_index = threadIdx.x + blockIdx.x * THREADS_PER_BLOCK;
+    int a_index = b_index * 2;
+    
+    if (b_index < n_output) {
+	    if (a_index == len) {
+	    	b[b_index] = a[a_index];
+	    }
+	    else {
+		    int v1 = a[a_index];
+		    int v2 = a[a_index + 1];
+		    if (v2 < v1) {
+		    	b[b_index] = v2;
+		    }
+		    else {
+		    	b[b_index] = v1;
+		    }
+		}
+	}
 }
 
 __global__ void last_digit_cuda(int* a, int* b, int len) {
@@ -14,6 +31,7 @@ __global__ void last_digit_cuda(int* a, int* b, int len) {
 }
 
 void last_digit() {
+	//Get input data from files
     char buff[50000];
     int inp[10000];
     buff[0] = ' ';
@@ -35,23 +53,36 @@ void last_digit() {
     	A[i] = inp[i];
     }
 
+    //Cuda stuff
     int *d_a, *d_b;
     cudaMalloc(&d_a, sizeof(int) * len);
     cudaMalloc(&d_b, sizeof(int) * len);
     cudaMemcpy(d_a, A, sizeof(int) * len, cudaMemcpyHostToDevice);
 
-    last_digit_cuda<<<len/THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_a, d_b, len);
+    last_digit_cuda<<<(len + THREADS_PER_BLOCK)/THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_a, d_b, len);
     cudaDeviceSynchronize();
     cudaMemcpy(B, d_b, sizeof(int) * len, cudaMemcpyDeviceToHost);
 
-    for(int i =0; i < len; i++) {
-    	printf("%d, ", B[i]);
-    }
+    //put results in file
+	fclose(fp);
+	FILE* fp_end = fopen("q1b.txt", "w");
+	for (int i = 0; i < len; i++) {
+		fputc(B[i] + '0', fp_end);
+		if (i != len-1) {
+			fputc(', ', fp_end);
+		}
+	}
 
-	printf("That's all!\n");
+	//Free up memory
+	cudaFree(d_a);
+	cudaFree(d_b);
+	free(A);
+	free(B);
+	fclose(fp_end);
 }
 
 void minA() {
+	//Get input from files
     char buff[50000];
     int inp[10000];
     buff[0] = ' ';
@@ -66,23 +97,51 @@ void minA() {
 		token = strtok(NULL, ",");
     }
 
+    //Edge case where there is only one element
+    if (len == 1) {
+    	printf("%d", inp[0]);
+    }
+
+    //Copy input to array of proper size
     int* A = (int* )malloc(sizeof(int) * len);
     for (int i = 0; i < len; i++) {
     	A[i] = inp[i];
     }
 
+    //Cuda stuff
+    int B_size = (len + 1) / 2;
+    int* B;
+
+    while (B_size != 0) {
+    	B = (int* )malloc(sizeof(int) * B_size);
+
+		int *d_a, *d_b;
+	    cudaMalloc(&d_a, sizeof(int) * len);
+	    cudaMalloc(&d_b, sizeof(int) * B_size);
+	    cudaMemcpy(d_a, A, sizeof(int) * len, cudaMemcpyHostToDevice);
+
+	    minA_cuda<<<(B_size + THREADS_PER_BLOCK)/THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_a, d_b, len, B_size);
+	    cudaDeviceSynchronize();
+	    cudaMemcpy(B, d_b, sizeof(int) * B_size, cudaMemcpyDeviceToHost);
+
+	    cudaFree(d_a);
+	    cudaFree(d_b);
+	    memcpy(A, B, B_size * sizezof(int));
+	    free(B);
+	    len = B_size;
+	    B_size = (len + 1) / 2;
+    }
+
+    //Print output to file
+    fclose(fp);
+	FILE* fp_end = fopen("q11.txt", "w");
+	fputc(A[0] + '0', fp_end);
+	fclose(fp_end);
 }
 
 int main(int argc,char **argv)
 {
-    // launch the kernel
-    //hello<<<NUM_BLOCKS, BLOCK_WIDTH>>>();
-
-    // force the printf()s to flush
-    //cudaDeviceSynchronize();
-
-    //printf("That's all!\n");
     last_digit();
-
+    minA();
     return 0;
 }
--- hw4/q2.cu
+++ hw4/q2.cu
@@ -8,12 +8,14 @@ __global__ void part_a_cuda(int* a, int* b, int len)
     int a_index = threadIdx.x + blockIdx.x * THREADS_PER_BLOCK;
     int b_index = 0;
     
-	if (a_index < len) 
+	if (a_index < len) {
 		b_index = a[a_index] / 100;
-		b[b_index]++; 
+		atomicAdd(&b[b_index], 1);
+	}
 }
 
 void part_a() {
+	//gather input from files
     char buff[50000];
     int inp[10000];
     buff[0] = ' ';
@@ -36,28 +38,38 @@ void part_a() {
     	A[i] = inp[i];
     }
 
+    //cuda stuff
     int *d_a, *d_b;
     cudaMalloc(&d_a, sizeof(int) * len);
     cudaMalloc(&d_b, sizeof(int) * 10);
     cudaMemcpy(d_a, A, sizeof(int) * len, cudaMemcpyHostToDevice);
-    part_a_cuda<<<len/THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_a, d_b, len);
+    part_a_cuda<<<(len + THREADS_PER_BLOCK)/THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_a, d_b, len);
     cudaDeviceSynchronize();
     cudaMemcpy(B, d_b, sizeof(int) * 10, cudaMemcpyDeviceToHost);
 
+    //Print final values
+    /*
     for(int i = 0; i < 10; i++) {
     	printf("%d, ", B[i]);
     }
-
+    */
+    fclose(fp);
+	FILE* fp_end = fopen("q2a.txt", "w");
+	for (int i = 0; i < 10; i++) {
+		fputc(B[i] + '0', fp_end);
+		if (i != 9) {
+			fputc(', ', fp_end);
+		}
+	}
+	fclose(fp_end);
+	cudaFree(d_a);
+	cudaFree(d_b);
 }
 
 int main(int argc, char **argv)
 {
     part_a();
-    printf("That's all for part a!\n");
-    //part_b()
-    //printf("That's all for part b!\n");
-    //part_c()
-    //printf("That's all for part c!\n");
-
+    part_b();
+    part_c();
     return 0;
 }
